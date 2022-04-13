a4c7364de67326a3778a72c47668731dca457aa0
Aman Deep <47072498+amandeep131313@users.noreply.github.com>
2022-04-13 17:52:54 +0530

    add change for files

--- src/urls.js
+++ src/urls.js
@@ -1,7 +1,6 @@
 const urls = [
    {label: 'url1', url: 'https://public.tableau.com/views/WorldIndicators/GDPpercapita'},
-   {label: 'url2', url: 'https://public.tableau.com/views/RegionalSampleWorkbook/Storms'},
-   {label: 'url3', url: 'https://public.tableau.com/app/profile/mikevizneros/viz/PoliticalDW/TheAmericanPoliticalUniverse'}
+   {label: 'url2', url: 'https://public.tableau.com/views/RegionalSampleWorkbook/Storms'}
 
 ]
 
